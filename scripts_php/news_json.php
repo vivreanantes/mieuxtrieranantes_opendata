094977c5fc5b1477b56ceb0a19cedb796137af62
Christian Renoulin <chris-ren@netcourrier.com>
2017-10-10 07:06:49 +0200

    Php

---
+++ scripts_php/news_json.php
@@ -0,0 +1,14 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+	print_r('[{
+    "code": "Actu_12",
+    "nom": "&#127873; 6 boîtes utiles à Nantes",
+    "nom_en": "&#127873; 6 give box in Nantes",
+    "descr": "Ce sont des endroits où l on dépose (ou prend) des petits objets, gratuitement. Posées en pleine rue, elles sont accessibles en permanence. Près de Vincent Gache, quartier Ste Thérèse, Bottière et maintenant Chanteay.",
+    "descr_en": "These are places where is deposited (or takes) small objects free. Asked in the street, they are always accessible. Vincent Gache, Ste Thérèse...",
+	"link":"https://nousandco.com/category/les-boites-utiles/"
+	}]');
+	
+?>
